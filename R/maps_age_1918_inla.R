9823036a9017dba2056101c08a8d102b12566e91
Katarina Matthes <katarina.matthes@iem.uzh.ch>
2022-09-14 10:43:37 +0200

    maps changed

--- R/maps_age_1918_inla.R
+++ R/maps_age_1918_inla.R
@@ -12,16 +12,16 @@ function_maps_age_1918 <- function(){
   # 
   # load(paste0("data/expected_death_inla70_1918.RData"))
   # death_1918_70 <- expected_deaths
-  
+  # 
   load(paste0("../data/expected_death_inla0_19_1918.RData"))
   death_1918_0_19 <- expected_deaths
-  
+
   load(paste0("../data/expected_death_inla20_39_1918.RData"))
   death_1918_20_39 <- expected_deaths
-  
+
   load(paste0("../data/expected_death_inla40_69_1918.RData"))
   death_1918_40_69 <- expected_deaths
-  
+
   load(paste0("../data/expected_death_inla70_1918.RData"))
   death_1918_70 <- expected_deaths
 
@@ -48,35 +48,56 @@ function_maps_age_1918 <- function(){
     dplyr::select(Bezirk, geometry) %>%
     full_join(data_excess) %>%
     mutate(excess_percentage_o = ((death-fit)/fit)*100,
+           excess_percentage_o = ifelse(excess_percentage_o <0,0, excess_percentage_o),
            excess_percentage = round(((death-fit)/fit)*100,2)) %>%
-    filter(!is.na(excess_percentage))  %>%
-    mutate(excess_perc_groups =  cut(excess_percentage, 
-                              breaks = c(-130, 0, 10, 20, 30, 40, 70, 180, 250,860), 
-                              labels = c("<0 %", "[0 %,10%)", "[10%, 20%)",
-                                         "[20 %, 30%)", "[30%, 40%)",  "[40%, 70%)",
-                                         "[70%, 180%)","[180%, 250%)",">250"),
-                              include.lowest = TRUE, right = FALSE))
+    filter(!is.na(excess_percentage)) 
+    # mutate(excess_perc_groups =  cut(excess_percentage, 
+    #                           breaks = c(-130, 0, 10, 20, 30, 40, 70, 180, 250,860), 
+    #                           labels = c("<0 %", "[0 %,10%)", "[10%, 20%)",
+    #                                      "[20 %, 30%)", "[30%, 40%)",  "[40%, 70%)",
+    #                                      "[70%, 180%)","[180%, 250%)",">250"),
+    #                           include.lowest = TRUE, right = FALSE))
+    # 
+  plot_excess <- tm_shape( bezirk_geo  ) + 
+    tm_fill("excess_percentage_o",
+            # palette = "Reds", 
+            # legend.hist = TRUE,
+            style = "jenks",
+            # style = "kmeans",
+            title = "Relative excess deaths",
+            legend.is.portrait = FALSE) +
+    tm_borders(alpha = 0.5)+
+    tm_facets(by="age_group", ncol=2)+
+    tm_layout(
+      #   main.title = "Relative excess deaths")
+      # main.title.position = "left",
+      # legend.text.size = legend_size_map,
+      # legend.width = 5,
+      # legend.height = 8,
+      # legend.position = c(0.7,0.8),
+      legend.outside.position = "bottom")
+  
     
-    plot_excess <- ggplot(data=bezirk_geo)+
-      # geom_sf_pattern(aes(pattern=significant_dummy, fill=excess_perc_groups),pattern_fill = "grey50", pattern_color="grey50",
-      #                 pattern_spacing = 0.03,pattern_size=0.5 )+
-      geom_sf(mapping = aes(fill =excess_perc_groups)) +
-      facet_wrap(~age_group, ncol = 2) +
-      scale_fill_manual("",values = c("#C6DBEF",col8viridis))+
-      scale_pattern_manual("significant",
-                           breaks =c("0", "1"),
-                           labels=c("no", "yes"),
-                           values = c("none","circle"))+
-      ggtitle("Relative excess deaths - 1918")+
-      theme(
-        panel.grid.major=element_blank(),
-        axis.title=element_blank(),
-        axis.line=element_blank(),
-        axis.text=element_blank(),
-        axis.ticks=element_blank(),
-        panel.border = element_blank(),
-        legend.position = "bottom")
-    
+    # plot_excess <- ggplot(data=bezirk_geo)+
+    #   # geom_sf_pattern(aes(pattern=significant_dummy, fill=excess_perc_groups),pattern_fill = "grey50", pattern_color="grey50",
+    #   #                 pattern_spacing = 0.03,pattern_size=0.5 )+
+    #   geom_sf(mapping = aes(fill =excess_perc_groups)) +
+    #   facet_wrap(~age_group, ncol = 2) +
+    #   scale_fill_manual("",values = c("#C6DBEF",col8viridis))+
+    #   scale_pattern_manual("significant",
+    #                        breaks =c("0", "1"),
+    #                        labels=c("no", "yes"),
+    #                        values = c("none","circle"))+
+    #   ggtitle("Relative excess deaths - 1918")+
+    #   theme(
+    #     panel.grid.major=element_blank(),
+    #     axis.title=element_blank(),
+    #     axis.line=element_blank(),
+    #     axis.text=element_blank(),
+    #     axis.ticks=element_blank(),
+    #     panel.border = element_blank(),
+    #     legend.position = "bottom")
+    # 
   # cowplot::save_plot("output/excess_rate_group_inla_test2.pdf",plot_excess,base_height=12,base_width=15)
     
     return(plot_excess)
--- R/maps_inla.R
+++ R/maps_inla.R
@@ -43,34 +43,60 @@ bezirk_geo <- read_sf("../data_raw/Map_2020/Maps_dissolved/Maps_dissolved_2020.s
   mutate(
           excess_percentage_o = ((death-fit)/fit)*100,
           excess_percentage = round(((death-fit)/fit)*100,2),
+          excess_percentage_o = ifelse(excess_percentage_o <0,0, excess_percentage_o))
           # excess_percentage = ifelse(excess_percentage<0, 0, excess_percentage),
           # excess_percentage = ifelse(excess_percentage>100, 100, excess_percentage),
-          excess_perc_groups =  cut(excess_percentage, 
-                                    breaks = c(-45, 0, 5, 10, 20, 40, 60,80,100,125), 
-                                    labels = c("<0 %", "[0 %,5%)", "[5%, 10%)",
-                                               "[10 %, 20%)", "[20%, 40%)",  "[40%, 60%)",
-                                               "[60%, 80%)","[80%, 100%)", ">100"),
-                                    include.lowest = TRUE, right = FALSE))
+          # excess_perc_groups =  cut(excess_percentage, 
+          #                           breaks = c(-45, 0, 5, 10, 20, 40, 60,80,100,125), 
+          #                           labels = c("<0 %", "[0 %,5%)", "[5%, 10%)",
+          #                                      "[10 %, 20%)", "[20%, 40%)",  "[40%, 60%)",
+          #                                      "[60%, 80%)","[80%, 100%)", ">100"),
+          #                           include.lowest = TRUE, right = FALSE))
+
+
+
+plot_excess <- tm_shape( bezirk_geo  ) + 
+  tm_fill("excess_percentage_o",
+           palette = "YlOrBr", 
+          # legend.hist = TRUE,
+          style = "jenks",
+          # style = "kmeans",
+          title = "Relative excess deaths",
+          legend.is.portrait = FALSE) +
+  tm_borders(alpha = 0.5)+
+  tm_facets(by="Year")+
+  tm_layout(
+  #   main.title = "Relative excess deaths")
+  # main.title.position = "left",
+  # legend.text.size = legend_size_map,
+  # legend.width = 5,
+  # legend.height = 8,
+  # legend.position = c(0.7,0.8),
+  legend.outside.position = "bottom")
+  # legend.outside.size = .1)
+  # legend.title.size=legend_size_map,
+  # main.title.size = main_size_map)
+
       
-plot_excess <- ggplot()+
-  # geom_sf_pattern(aes(pattern=significant_dummy, fill=excess_rate_group),pattern_fill = "grey50", pattern_color="grey50",
-  #                 pattern_spacing = 0.03,pattern_size=0.5 )+
-  geom_sf(data= bezirk_geo, aes(fill=  excess_perc_groups ),alpha=1,col="black", size=0.1) +
-  facet_wrap(~Year, ncol = 2) +
-  scale_fill_manual("",values = c("#C6DBEF",col8viridis))+
-  scale_pattern_manual("significant",
-                       breaks =c("0", "1"),
-                       labels=c("no", "yes"),
-                       values = c("none","circle"))+
-  ggtitle("Relative excess deaths")+
-  theme(
-    panel.grid.major=element_blank(),
-    axis.title=element_blank(),
-    axis.line=element_blank(),
-    axis.text=element_blank(),
-    axis.ticks=element_blank(),
-    panel.border = element_blank(),
-    legend.position = "bottom")
+# plot_excess <- ggplot()+
+#   # geom_sf_pattern(aes(pattern=significant_dummy, fill=excess_rate_group),pattern_fill = "grey50", pattern_color="grey50",
+#   #                 pattern_spacing = 0.03,pattern_size=0.5 )+
+#   geom_sf(data= bezirk_geo, aes(fill=  excess_perc_groups ),alpha=1,col="black", size=0.1) +
+#   facet_wrap(~Year, ncol = 2) +
+#   scale_fill_manual("",values = c("#C6DBEF",col8viridis))+
+#   scale_pattern_manual("significant",
+#                        breaks =c("0", "1"),
+#                        labels=c("no", "yes"),
+#                        values = c("none","circle"))+
+#   ggtitle("Relative excess deaths")+
+#   theme(
+#     panel.grid.major=element_blank(),
+#     axis.title=element_blank(),
+#     axis.line=element_blank(),
+#     axis.text=element_blank(),
+#     axis.ticks=element_blank(),
+#     panel.border = element_blank(),
+#     legend.position = "bottom")
 # cowplot::save_plot("output/excess_rate_group_inla_test2.pdf",plot_excess,base_height=12,base_width=10)
 
 return(plot_excess)
